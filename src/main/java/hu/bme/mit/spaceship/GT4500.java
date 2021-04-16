b13a8ed284dc1cd337298871c089e244c0deb3b4
CYA7E4 <CYA7E4@example.com>
2021-04-16 15:20:59 +0200

    Add merge conflict comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,6 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
+        // Merge conflict comment
         boolean oneSucceed = false;
         if (! primaryTorpedoStore.isEmpty()) {
           oneSucceed = primaryTorpedoStore.fire(1);
